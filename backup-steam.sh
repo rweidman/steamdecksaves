42d89152cb92d7c28381b166e4c0c94a4f49ba8b
Ryan Weidman <rweidman@outlook.com>
2024-08-12 23:47:29 -0500

    Backed up steam save files on Mon Aug 12 11:47:29 PM CDT 2024

--- backup-steam.sh
+++ backup-steam.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash 
-/usr/bin/rsync -av -e ssh --exclude="windows" --exclude="Program Files" --exclude="ProgramData" --exclude="Program Files (x86)"  deck@192.168.1.72:/home/deck/steamsaves/* ./
+/usr/bin/rsync -av -e ssh --exclude="windows" --exclude="Program Files" --exclude="ProgramData" --exclude="Program Files (x86)"  deck@192.168.1.72:/home/deck/steamsaves/* /home/garmanarnar/steamsaves/
+cd /home/deck/garmanarnar
 /usr/bin/git add .
 /usr/bin/git commit -m "Backed up steam save files on $(date)"
 /usr/bin/git push main
